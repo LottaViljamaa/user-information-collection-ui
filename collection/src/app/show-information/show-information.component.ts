f238d7169da343e25e684a46b06541266728cb53
LottaViljamaa <lotta.viljamaa@tuni.fi>
2024-09-25 19:08:14 +0300

    Tietyn henkilön henkilötietoja voi tarkastella antamalla käyttäjän henkilötunnuksen.

--- collection/src/app/show-information/show-information.component.ts
+++ collection/src/app/show-information/show-information.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-show-information',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './show-information.component.html',
   styleUrl: './show-information.component.css'
 })
 export class ShowInformationComponent {
+personalIdentityCode: string = '';
+  user: any;
+  errorMessage: string = '';
 
+  private apiUrl = 'http://localhost:8090/users/get';
+
+  constructor(private http: HttpClient) {}
+
+  onSearch() {
+    this.user = null;
+    this.errorMessage = '';
+
+    if (this.personalIdentityCode) {
+      this.http.get(`${this.apiUrl}/${this.personalIdentityCode}`)
+        .pipe(
+          catchError(err => {
+            this.errorMessage = 'Annetulla henkilötunnuksella ei löydy tietoja.';
+            return of(null);
+          })
+        )
+        .subscribe((data: any) => {
+          if (data) {
+            this.user = data;
+          }
+        });
+    } else {
+      this.errorMessage = 'Henkilötunnus on pakollinen.';
+    }
+  }
 }
