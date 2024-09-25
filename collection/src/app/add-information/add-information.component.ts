0f2e7071648c87a39feea251707c6d28116b96c0
LottaViljamaa <lotta.viljamaa@tuni.fi>
2024-09-25 13:56:03 +0300

    Lisätty kenttä etunimen syöttämistä varten ja lähetä -painike.

--- collection/src/app/add-information/add-information.component.ts
+++ collection/src/app/add-information/add-information.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-add-information',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './add-information.component.html',
   styleUrl: './add-information.component.css'
 })
-export class AddInformationComponent {
- 
+export class AddInformationComponent implements OnInit {
+  informationForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    // Lomake, jossa vain yksi kenttä
+    this.informationForm = this.fb.group({
+      firstName: ['', Validators.required]
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    if (this.informationForm.valid) {
+      console.log('Form Submitted', this.informationForm.value);
+    }
+  }
 }
