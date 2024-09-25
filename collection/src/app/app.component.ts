4b861983d4a568dcae06c133ce82475d3061d228
LottaViljamaa <lotta.viljamaa@tuni.fi>
2024-09-25 18:31:13 +0300

    Kaikkien henkilötiedot pystyy hakemaan taustapalvelusta.

--- collection/src/app/app.component.ts
+++ collection/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, RouterModule],
+  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, RouterModule, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- collection/src/app/show-informations/show-informations.component.ts
+++ collection/src/app/show-informations/show-informations.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError, of } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-show-informations',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './show-informations.component.html',
   styleUrl: './show-informations.component.css'
 })
-export class ShowInformationsComponent {
+export class ShowInformationsComponent implements OnInit {
+  users: any[] = [];
+  private apiUrl = 'http://localhost:8090/users/all';
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.fetchUsers();
+  }
+
+
+  fetchUsers(): void {
+    this.http.get<any[]>(this.apiUrl)
+      .pipe(
+        catchError(error => {
+          console.error('Virhe käyttäjätietojen haussa:', error); 
+          alert('Tietojen haussa tapahtui virhe.');
+          return of([]);
+        })
+      )
+      .subscribe(data => {
+        console.log('Käyttäjätiedot:', data);
+        this.users = data;
+      });
+  }
+  
 }
