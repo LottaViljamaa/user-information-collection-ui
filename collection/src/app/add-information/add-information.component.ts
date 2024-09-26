2d693bfd3b7ce4f15124e7b927652db5872e0a83
LottaViljamaa <lotta.viljamaa@tuni.fi>
2024-09-26 19:21:42 +0300

    Henkilötietojen lisäys onnistuu.

--- collection/src/app/add-information/add-information.component.ts
+++ collection/src/app/add-information/add-information.component.ts
@@ -1,40 +1,67 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { FormBuilder, FormGroup, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
-
 
 @Component({
   selector: 'app-add-information',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './add-information.component.html',
   styleUrl: './add-information.component.css'
 })
 export class AddInformationComponent implements OnInit {
-  informationForm: FormGroup;
+  informationForm!: FormGroup; 
 
-  constructor(private fb: FormBuilder) {
-    this.informationForm = this.fb.group({
-      firstName: ['', [Validators.required, Validators.pattern('^[a-zA-Z-]+$')]],
-      lastName: ['', [Validators.required, Validators.pattern('^[a-zA-Z-]+$')]],
-      personallIdentityCode: ['', [Validators.required, Validators.pattern('^\\d{6}-\\d{4}$')]],
-      citizenship: ['', [Validators.required, Validators.pattern('^[a-zA-Z]+$')]],
-      gender: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      phoneNumber: ['', [Validators.required, Validators.pattern('^[+]?[0-9]*$'), Validators.maxLength(20)]],
-      streetAddress: ['', Validators.required],
-      city: ['', Validators.required],
-      postalCode: ['', [Validators.required, Validators.pattern('^\\d{5}$')]],
-    });
-  }
+  private apiUrl = 'http://localhost:8090/users/add';
+
+  constructor(private fb: FormBuilder, private http: HttpClient) {}
 
   ngOnInit(): void {
+    this.informationForm = this.fb.group({
+      firstName: ['Matti', [Validators.required, Validators.pattern('^[a-zA-Z-]+$')]],
+      lastName: ['Meikalainen', [Validators.required, Validators.pattern('^[a-zA-Z-]+$')]],
+      personalIdentityCode: ['000000-0000', [Validators.required, Validators.pattern('^\\d{6}-\\d{4}$')]],
+      citizenship: ['Suomi', [Validators.required, Validators.pattern('^[a-zA-Z]+$')]],
+      gender: ['Mies', Validators.required],
+      email: ['matti.meikalainen@esimerkki.com', [Validators.required, Validators.email]],
+      phoneNumber: ['044123456', [Validators.required, Validators.pattern('^[+]?[0-9]*$'), Validators.maxLength(20)]],
+      streetAddress: ['Esimerkkikatu', Validators.required],
+      city: ['Esimerkki', Validators.required],
+      postalCode: ['00000', [Validators.required, Validators.pattern('^\\d{5}$')]]
+    });
   }
 
   onSubmit(): void {
     if (this.informationForm.valid) {
-      alert("Tiedot lähetetty onnistuneesti.");
-      console.log('Form Submitted', this.informationForm.value);
+      const basicInformation = {
+        firstName: this.informationForm.get('firstName')?.value,
+        surname: this.informationForm.get('lastName')?.value,
+        personalIdentityCode: this.informationForm.get('personalIdentityCode')?.value,
+        citizenship: this.informationForm.get('citizenship')?.value,
+        gender: this.informationForm.get('gender')?.value
+      };
+
+      const contactInformation = {
+        personalIdentityCode: this.informationForm.get('personalIdentityCode')?.value,
+        email: this.informationForm.get('email')?.value,
+        phoneNumber: this.informationForm.get('phoneNumber')?.value,
+        streetAddress: this.informationForm.get('streetAddress')?.value,
+        city: this.informationForm.get('city')?.value,
+        postalCode: this.informationForm.get('postalCode')?.value
+      };
+
+      const userData = {
+        basicInformation: basicInformation,
+        contactInformation: contactInformation
+      };
+
+      this.http.put(this.apiUrl, userData)
+      .subscribe(() => {
+        console.log('Tietojen lähetys onnistui.');
+      });
     }
   }
 }
